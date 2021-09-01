63f4b4116b175817dda4be881a671a6b32ea297a
TechVortexYT <geezgamer24@gmail.com>
2021-09-01 11:02:35 +0800

    0.0.1|added colours and logo and changed title, alex has left

--- Pygame.py
+++ Pygame.py
@@ -1,7 +1,15 @@
 import pygame
 pygame.init()
-
+red = (255, 0, 0)
+pygame.display.set_caption("The Ultimate Game")
+icon = pygame.image.load('download.png')
+pygame.display.set_icon(icon)
 screen = pygame.display.set_mode((1024, 720))
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-while True:
-    pass
+    screen.fill((red))
+    pygame.display.update()
